52c1453bb3413fe70628a4b188b9a3e09e7b3659
vrpro <topicgames08@gmail.com>
2026-05-19 18:18:00 +0300

    Added show_books, average_rating, author_stats functions

--- main.py
+++ main.py
@@ -14,3 +14,20 @@ def load_books():
 def save_books(books):
     with open(FILE, "w", encoding="utf-8") as f:
         json.dump(books, f, ensure_ascii=False, indent=2)
+
+def show_books(books):
+    for i, b in enumerate(books):
+        print(f"{i+1}. {b['author']} — {b['title']} ({b['rating']})")
+
+
+def average_rating(books):
+    if not books:
+        return 0
+    return sum(b["rating"] for b in books) / len(books)
+
+
+def author_stats(books):
+    stats = {}
+    for b in books:
+        stats[b["author"]] = stats.get(b["author"], 0) + 1
+    return stats
