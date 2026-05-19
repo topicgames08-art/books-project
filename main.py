0b120c91845e7f338db04d88e5343c866b81479b
vrpro <topicgames08@gmail.com>
2026-05-19 18:19:15 +0300

    Added delete_book function

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ def load_books():
 def save_books(books):
     with open(FILE, "w", encoding="utf-8") as f:
         json.dump(books, f, ensure_ascii=False, indent=2)
+
+def delete_book(books):
+    for i, b in enumerate(books):
+        print(f"{i+1}. {b['author']} — {b['title']}")
+
+    idx = int(input("Номер книги: ")) - 1
+
+    if 0 <= idx < len(books):
+        books.pop(idx)
+    else:
+        print("Неверный номер")
